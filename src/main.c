70978841aace4f9879fed2d0b9f02e4f19581d15
yaraikun <evan.calupig@gmail.com>
2024-11-12 02:53:47 +0800

    docs(main): Created specs tldr, and program flow.

--- src/main.c
+++ src/main.c
@@ -1,3 +1,45 @@
+/**
+ * SPECS TL;DR
+ *
+ * Produce Energon at the start of each week.
+ *
+ * Randomly generate the costs
+ * - to produce energon cubes
+ * - to sell energon cubes
+ * 
+ * NOTE: Prices are influenced by a weekly trend.
+ *
+ * Track inventory and Energon balance.
+ *
+ * Sell cubes daily
+ *
+ * Recycle unsold cubes.
+ *
+ * Goal: Reach 1 million cubes in 10 weeks. Or just die a painful and poor
+ *       death.
+ */
+
+/**
+ * PROGRAM FLOW
+ *
+ * 1. Initialize game
+ *    - Set Soundwave's energon to 10,000 and the week to 1.
+ *
+ * 2. Weekly Loop:
+ *    - 10 iterations, one per week.
+ *    - Get user input for number of stacks.
+ *      - 1 Stack = 10 Energon
+ *    - Track produced stacks.
+ *    - Subtract production cost from Soundwave's energon.
+ *    - Daily Loop:
+ *      - Generate random price for cubes according to trend.
+ *      - Allow user to choose to sell a stack.
+ *    - Recycle cubes
+ * 3. End game
+ *    - After 10 Weeks.
+ *    - If too poor.
+ */
+
 #include <stdio.h>
 
 int main(void)
